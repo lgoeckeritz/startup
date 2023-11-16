a19aeb5f11f31ee201492d6ecaf0b10df7fc5607
Layton Goeckeritz <laygoeckeritz@gmail.com>
2023-11-15 18:07:43 -0700

    adding login functionality

--- database.js
+++ database.js
@@ -1,9 +1,12 @@
 const { MongoClient } = require('mongodb');
+const bcrypt = require('bcrypt');
+const uuid = require('uuid');
 const config = require('./dbConfig.json');
 
 const url = `mongodb+srv://${config.userName}:${config.password}@${config.hostname}`;
 const client = new MongoClient(url);
 const db = client.db('startup');
+const userCollection = db.collection('user');
 const recipeCollection = db.collection('recipes');
 
 // This will asynchronously test the connection and exit the process if it fails
@@ -15,6 +18,29 @@ const recipeCollection = db.collection('recipes');
   process.exit(1);
 });
 
+function getUser(email) {
+  return userCollection.findOne({ email: email });
+}
+
+function getUserByToken(token) {
+  return userCollection.findOne({ token: token });
+}
+
+async function createUser(name, email, password) {
+  // Hash the password before we insert it into the database
+  const passwordHash = await bcrypt.hash(password, 10);
+
+  const user = {
+    name: name,
+    email: email,
+    password: passwordHash,
+    token: uuid.v4(),
+  };
+  await userCollection.insertOne(user);
+
+  return user;
+}
+
 async function addRecipe(recipe) {
   const result = await recipeCollection.insertOne(recipe);
   return result;
@@ -25,4 +51,10 @@ function getRecipes() {
   return cursor.toArray();
 }
 
-module.exports = { addRecipe, getRecipes };
+module.exports = { 
+  getUser,
+  getUserByToken,
+  createUser,
+  addRecipe, 
+  getRecipes 
+};
